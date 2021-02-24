53e976a5723a4c5841a0378472ee52b409c6c68e
sumanahmed <suman777333@gmail.com>
2021-02-24 23:37:19 +0600

    file folder separate for admin panel mastering

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/admin', function () {
+    Route::get('/', [DashboardController::class, 'dashboard']);
 });
