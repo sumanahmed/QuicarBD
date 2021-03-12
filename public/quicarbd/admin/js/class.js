20a293d323d001ead1be1d48bb56f72e1ff2dcaa
sumanahmed <suman777333@gmail.com>
2021-03-12 07:07:24 +0600

    modify to remove car_type_id

--- public/quicarbd/admin/js/class.js
+++ public/quicarbd/admin/js/class.js
@@ -3,14 +3,12 @@
 $("#create").click(function (e) {
     e.preventDefault();
     var name = $("#name").val();
-    var car_type_id = $("#car_type_id :selected").val();
     $.ajax({
         type:'POST',
         url: '/admin/class/store',
         headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') },
         data: {
-            name : name,
-            car_type_id : car_type_id,
+            name : name,            
         },
         success:function(response){
             if((response.errors)){
@@ -23,10 +21,9 @@ $("#create").click(function (e) {
                 $('#createClassModal').modal('hide');
                 $("#allClass").append('' +
                     '<tr class="Class-'+ response.data.id +'">\n' +
-                        '<td>'+ response.data.value +'</td>\n' +
-                        '<td>'+ response.data.car_type_name +'</td>\n' +
+                        '<td>'+ response.data.value +'</td>\n' +                        
                         '<td style="vertical-align: middle;text-align: center;">\n' +                        
-                            '<button class="btn btn-xs btn-warning" data-toggle="modal" id="editClass" data-target="#editClassModal" data-id="'+ response.data.id +'" data-name="'+ response.data.value +'" data-car_type_id="'+ response.data.car_type_id +'" title="Edit"><i class="fa fa-edit"></i></button>\n' +
+                            '<button class="btn btn-xs btn-warning" data-toggle="modal" id="editClass" data-target="#editClassModal" data-id="'+ response.data.id +'" data-name="'+ response.data.value +'" title="Edit"><i class="fa fa-edit"></i></button>\n' +
                             '<button class="btn btn-xs btn-danger" data-toggle="modal" id="deleteClass" data-target="#deleteClassModal" data-id="'+ response.data.id +'" title="Delete"><i class="fa fa-remove"></i></button>\n' +
                         '</td>\n' +
                     '</tr>'+
@@ -44,7 +41,6 @@ $(document).on('click', '#editClass', function () {
     $('#editClassModal').modal('show');
     $('#edit_id').val($(this).data('id'));
     $('#edit_name').val($(this).data('name'));
-    $('#edit_car_type_id').val($(this).data('car_type_id'));
  });
 
 // update Class
@@ -52,7 +48,6 @@ $("#update").click(function (e) {
     e.preventDefault();
     var id      = $("#edit_id").val();
     var name    = $("#edit_name").val();
-    var car_type_id    = $("#edit_car_type_id :selected").val();
     $.ajax({
         type:'POST',
         url: '/admin/class/update',
@@ -60,7 +55,6 @@ $("#update").click(function (e) {
         data: {
             id    : id,
             name  : name,
-            car_type_id  : car_type_id,
         },
         success:function(response){
             if((response.errors)){
@@ -74,9 +68,8 @@ $("#update").click(function (e) {
                 $("tr.Class-"+ response.data.id).replaceWith('' +
                     '<tr class="Class-'+ response.data.id +'">\n' +
                         '<td>'+ response.data.value +'</td>\n' +
-                        '<td>'+ response.data.car_type_name +'</td>\n' +
                         '<td style="vertical-align: middle;text-align: center;">\n' +
-                            '<button class="btn btn-xs btn-warning" data-toggle="modal" id="editClass" data-target="#editClassModal" data-id="'+ response.data.id +'" data-name="'+ response.data.value +'" data-car_type_id="'+ response.data.car_type_id +'" title="Edit"><i class="fa fa-edit"></i></button>\n' +
+                            '<button class="btn btn-xs btn-warning" data-toggle="modal" id="editClass" data-target="#editClassModal" data-id="'+ response.data.id +'" data-name="'+ response.data.value +'" title="Edit"><i class="fa fa-edit"></i></button>\n' +
                             '<button class="btn btn-xs btn-danger" data-toggle="modal" id="deleteClass" data-target="#deleteClassModal" data-id="'+ response.data.id +'" title="Delete"><i class="fa fa-remove"></i></button>\n' +
                         '</td>\n' +
                     '</tr>'+
